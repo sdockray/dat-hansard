f2cab229eb0716d7bfe2cc52b5146741a7a21495
sdockray <sean.patrick.dockray@gmail.com>
2017-11-03 14:09:48 +1100

    uncomment hansard backups

--- cli.js
+++ cli.js
@@ -5,10 +5,10 @@ var h2d = require('./index.js');
 console.log('Running Hansard -> Dat');
 
 cli
-  .version('1.0.1')
+  .version('1.0.2')
   .option('-d, --directory <directory>', 'The directory to save the data to')
   .parse(process.argv);
 
-// h2d.representativesToDat(cli.directory);
-// h2d.senateToDat(cli.directory);
+h2d.representativesToDat(cli.directory);
+h2d.senateToDat(cli.directory);
 h2d.membersToDat(cli.directory);
